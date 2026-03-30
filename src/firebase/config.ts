a98167f0fb67e2e23b04261b8711c4331bffdf85
MOHAMED HAFEEZ A btech_it2023 <230081601039@crescent.education>
2026-03-30 15:47:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7382334846-a6c1b",
+  "appId": "1:803847599763:web:da50b394052dee55af3f66",
+  "apiKey": "AIzaSyBwLIsThTS7nn0ZFnNZqUhXqaTeQZ6UE3o",
+  "authDomain": "studio-7382334846-a6c1b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "803847599763"
+};
